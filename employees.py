8572843da84039a413c08de7de5651756c503ac8
Bernalyn Benedicto <benedictobernalyn52@gmail.com>
2024-06-13 14:36:43 +0800

    I Love Programming

--- employees.py
+++ employees.py
@@ -2,10 +2,49 @@ from connector import Connector
 
 class Employee:
 
+    @staticmethod
     def get_all():
         query = "SELECT * FROM employees"
-
         Connector.cursor.execute(query)
         result = Connector.cursor.fetchall()
-
         return result
+
+    @staticmethod
+    def get_by_id(emp_id):
+        query = "SELECT * FROM employees WHERE emp_id = %s"
+        Connector.cursor.execute(query, (emp_id,))
+        result = Connector.cursor.fetchone()
+        return result
+
+    @staticmethod
+    def add_employee(emp_id, lname, fname, mname):
+        query = "INSERT INTO employees (emp_id, lname, fname, mname) VALUES (%s, %s, %s, %s)"
+        try:
+            Connector.cursor.execute(query, (emp_id, lname, fname, mname))
+            Connector.db.commit()
+            return True
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
+
+    @staticmethod
+    def update_employee(emp_id, lname, fname, mname):
+        query = "UPDATE employees SET lname = %s, fname = %s, mname = %s WHERE emp_id = %s"
+        try:
+            Connector.cursor.execute(query, (lname, fname, mname, emp_id))
+            Connector.db.commit()
+            return True
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
+
+    @staticmethod
+    def delete_employee(emp_id):
+        query = "DELETE FROM employees WHERE emp_id = %s"
+        try:
+            Connector.cursor.execute(query, (emp_id,))
+            Connector.db.commit()
+            return True
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from users import Users
 from employees import Employee
 
 app = Flask(__name__)
+app.secret_key = "sabbak"
 
 @app.route('/')
 def index():
@@ -18,12 +19,65 @@ def check_user():
     if result:
         return redirect('/employee-list')
     else:
-        return render_template('index.html')
+        session['message'] = "Invalid username or password"
+        return redirect('/')
 
 @app.route('/employee-list')
 def employee_list():
     employees = Employee.get_all()
-    return render_template('employee_list.html', employees=employees)
+    message = session.pop('message', '')
+    return render_template('employee_list.html', employees=employees, message=message)
+
+@app.route('/add-form')
+def add_form():
+    return render_template('add_employee.html')
+
+@app.route('/add-employee', methods=['POST'])
+def add_employee():
+    emp_id = request.form["emp_id"]
+    lname = request.form["lname"]
+    fname = request.form["fname"]
+    mname = request.form["mname"]
+
+    success = Employee.add_employee(emp_id, lname, fname, mname)
+
+    if success:
+        session["message"] = "Successfully added"
+    else:
+        session["message"] = "Failed to add employee"
+
+    return redirect('/employee-list')
+
+@app.route('/update-form/<emp_id>')
+def update_form(emp_id):
+    employee = Employee.get_by_id(emp_id)
+    return render_template('update_employee.html', employee=employee)
+
+@app.route('/update-employee/<emp_id>', methods=['POST'])
+def update_employee(emp_id):
+    lname = request.form["lname"]
+    fname = request.form["fname"]
+    mname = request.form["mname"]
+
+    success = Employee.update_employee(emp_id, lname, fname, mname)
+
+    if success:
+        session["message"] = "Successfully updated"
+    else:
+        session["message"] = "Failed to update employee"
+
+    return redirect('/employee-list')
+
+@app.route('/delete-employee/<emp_id>')
+def delete_employee(emp_id):
+    success = Employee.delete_employee(emp_id)
+
+    if success:
+        session["message"] = "Successfully deleted"
+    else:
+        session["message"] = "Failed to delete employee"
+
+    return redirect('/employee-list')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
